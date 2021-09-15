fbba986c35e254355f7a1bd0c9154fdcf46e9b57
maryano1096 <maryano1096@gmail.com>
2021-09-15 11:45:10 +0300

    last

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Table from './App';
-import Pages from './assets/languages/Pages';
+import Pages from './Pages.js';
 import './i18nextConf';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 
